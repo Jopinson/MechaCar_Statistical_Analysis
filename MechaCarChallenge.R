b57577a8041ec61e015f22ac612f331be2435530
Jopinson <68392225+Jopinson@users.noreply.github.com>
2020-10-29 13:28:36 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,13 @@ MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactor
 MechaCarData <- MechaCar_table
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData)) #generate summary statistics
+
+Suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- Suspension_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+
+
+
+
+
